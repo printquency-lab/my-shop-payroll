dc34879dc704b7f4c758a2cfdf23e5b7c65cf53f
pq <printquency@gmail.com>
2026-02-17 17:12:35 +0800

    Update oncall.py

--- oncall.py
+++ oncall.py
@@ -116,3 +116,4 @@ if st.query_params.get("view") == "hmaxine" or (admin_mode and admin_pw == "Hmax
         st.link_button("📂 Open Drive Photos", f"https://drive.google.com/drive/folders/{DRIVE_FOLDER_ID}")
     except:
         st.info("Awaiting records...")
+
